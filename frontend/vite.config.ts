9333e34ce792c1be0af56339edda639b22b9b990
Milena Vicente <milena.vicente@dfl.com.br>
2026-05-05 05:49:41 -0300

    corrige alias do vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { fileURLToPath, URL } from "node:url"
+
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 5173,
+  },
 })
