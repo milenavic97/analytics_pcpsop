1a2377e39dedc98c954c8697c39c0ce333c4c9a6
milenavic97 <milenavicente1@outlook.com>
2026-05-13 08:26:37 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
 import { ProtectedRoute } from "./components/ProtectedRoute"
+
 import { LoginPage } from "./pages/Login"
 import { OverviewPage } from "./pages/Overview"
 import { ProducaoPage } from "./pages/Producao"
 import { DadosPage } from "./pages/Dados"
 import { OrdensPage } from "./pages/Ordens"
+
+import { CalendarioParadasPage } from "./pages/calendario-paradas"
+
 import { Layout } from "./components/layout/Layout"
 
 export default function App() {
@@ -23,24 +28,52 @@ export default function App() {
           }
         >
           {/* Overview */}
-          <Route path="/overview" element={<OverviewPage />} />
+          <Route
+            path="/overview"
+            element={<OverviewPage />}
+          />
 
           {/* Produção */}
-          <Route path="/producao/*" element={<ProducaoPage />} />
+          <Route
+            path="/producao/*"
+            element={<ProducaoPage />}
+          />
 
-          {/* Ordens de Produção */}
-          <Route path="/ordens" element={<OrdensPage />} />
+          {/* Ordens */}
+          <Route
+            path="/ordens"
+            element={<OrdensPage />}
+          />
 
-          {/* Dados (IMPORTANTE) */}
-          <Route path="/dados" element={<DadosPage />} />
-          <Route path="/dados/:baseId" element={<DadosPage />} />
+          {/* Calendário de Paradas */}
+          <Route
+            path="/calendario-paradas"
+            element={<CalendarioParadasPage />}
+          />
+
+          {/* Dados */}
+          <Route
+            path="/dados"
+            element={<DadosPage />}
+          />
+
+          <Route
+            path="/dados/:baseId"
+            element={<DadosPage />}
+          />
         </Route>
 
         {/* ROOT */}
-        <Route path="/" element={<Navigate to="/overview" replace />} />
+        <Route
+          path="/"
+          element={<Navigate to="/overview" replace />}
+        />
 
         {/* FALLBACK */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route
+          path="*"
+          element={<Navigate to="/login" replace />}
+        />
       </Routes>
     </BrowserRouter>
   )
