8937dfc97ffc8ca6cd3e116c76e4474089b5c14d
milenavic97 <milenavicente1@outlook.com>
2026-05-23 21:09:48 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import { ProtectedRoute } from "./components/ProtectedRoute"
 
+import { AuthProvider } from "./contexts/AuthContext"
+
 import { LoginPage } from "./pages/Login"
+
 import { OverviewPage } from "./pages/Overview"
 import { ProducaoPage } from "./pages/Producao"
 import { DadosPage } from "./pages/Dados"
 import { OrdensPage } from "./pages/Ordens"
+
 import Mrp from "./pages/Mrp"
 import AnaliseMrpPage from "./pages/AnaliseMrp"
 
@@ -16,34 +20,101 @@ import { Layout } from "./components/layout/Layout"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          element={
-            <ProtectedRoute>
-              <Layout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="/overview" element={<OverviewPage />} />
-          <Route path="/producao/*" element={<ProducaoPage />} />
-          <Route path="/ordens" element={<OrdensPage />} />
+          <Route
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            <Route
+              path="/overview"
+              element={
+                <ProtectedRoute permissao="overview">
+                  <OverviewPage />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route path="/mps" element={<Mrp />} />
-          <Route path="/mrp" element={<Navigate to="/mps" replace />} />
+            <Route
+              path="/producao/*"
+              element={
+                <ProtectedRoute permissao="producao">
+                  <ProducaoPage />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route path="/analise-mrp" element={<AnaliseMrpPage />} />
+            <Route
+              path="/ordens"
+              element={
+                <ProtectedRoute permissao="ordens">
+                  <OrdensPage />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route path="/calendario-paradas" element={<CalendarioParadasPage />} />
-          <Route path="/dados" element={<DadosPage />} />
-          <Route path="/dados/:baseId" element={<DadosPage />} />
-        </Route>
+            <Route
+              path="/mps"
+              element={
+                <ProtectedRoute permissao="mps">
+                  <Mrp />
+                </ProtectedRoute>
+              }
+            />
 
-        <Route path="/" element={<Navigate to="/overview" replace />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
+            <Route
+              path="/mrp"
+              element={<Navigate to="/mps" replace />}
+            />
+
+            <Route
+              path="/analise-mrp"
+              element={
+                <ProtectedRoute permissao="analise-mrp">
+                  <AnaliseMrpPage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/calendario-paradas"
+              element={
+                <ProtectedRoute permissao="calendario-paradas">
+                  <CalendarioParadasPage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/dados"
+              element={
+                <ProtectedRoute permissao="dados">
+                  <DadosPage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/dados/:baseId"
+              element={
+                <ProtectedRoute permissao="dados">
+                  <DadosPage />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+
+          <Route path="/" element={<Navigate to="/overview" replace />} />
+
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
