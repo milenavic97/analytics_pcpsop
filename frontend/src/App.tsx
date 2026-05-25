2b45fcc4ef4fdee910ea573ab283af8c3727aaa0
milenavic97 <milenavicente1@outlook.com>
2026-05-25 15:57:42 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,7 +52,7 @@ export default function App() {
             <Route
               path="/faturamento"
               element={
-                <ProtectedRoute permissao="faturamento">
+                <ProtectedRoute>
                   <FaturamentoPage />
                 </ProtectedRoute>
               }
